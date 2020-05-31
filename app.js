eb40dc29058fe5fa25c1c60f0ebb46ef1420e8c6
CodewithKisha <48165215+CodewithKisha@users.noreply.github.com>
2020-05-31 10:21:42 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+//function
+function showTime() {
+
+//variables
+    let date = new Date();
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+    let session = "AM";
+
+//Conditional Statement
+    if(h===0){
+        h = 12;
+    }
+
+    if(h > 12){
+        h = h-12;
+        session = "PM";
+    }
+
+    h = (h<10) ? "0" + h:h;
+    m = (m<10) ? "0" + m:m;
+    s = (s<10) ? "0" + s:s;
+    
+    let time = h + " " + ":" + " " + m + " " + " : " + s + " " + session;
+//selectors 
+    document.getElementById("clockDisplay").innerText = time;
+    document.getElementById("clockDisplay").textContent = time;
+    setTimeout(showTime, 1000);
+}
+showTime();
